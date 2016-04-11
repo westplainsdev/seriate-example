5a23ca27adcb66bc5c51c6867bb34a72aef8a0ab
Chris Berry <chris.berry07@gmail.com>
2016-04-11 15:42:00 -0400

    Update hello-sql.js

--- hello-sql.js
+++ hello-sql.js
@@ -1,4 +1,3 @@
-// https://keyholesoftware.com/2016/04/04/seriate-the-glue-between-sql-server-and-node-js/
 // http://developer.leankit.com/painless-sql-server-with-nodejs-and-seriate/
 // https://www.npmjs.com/package/seriate
 
